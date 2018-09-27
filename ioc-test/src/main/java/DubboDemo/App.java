d1666ef5efb2d3f8a5720a74eb2301ed47e7068c
xzcai <82148941@qq.com>
2018-09-27 19:22:12 +0800

    Update App.java

--- ioc-test/src/main/java/DubboDemo/App.java
+++ ioc-test/src/main/java/DubboDemo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        int age = 12;
         System.out.println( "Hello World!" );
         int i=10;
         String age = "123";

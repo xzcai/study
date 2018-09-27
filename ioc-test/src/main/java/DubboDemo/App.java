667b177818056e2b52dbcef737750804c4cc7f7c
ywwl <mifang@ywwl.com>
2018-09-27 20:45:47 +0800

    git stash

--- ioc-test/src/main/java/DubboDemo/App.java
+++ ioc-test/src/main/java/DubboDemo/App.java
@@ -17,6 +17,7 @@ public class App
      */
     public static void main( String[] args )
     {
+        //bug
         int age = 12;
         System.out.println( "Hello World!" );
         int i=10;

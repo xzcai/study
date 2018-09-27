e37ff3808552cf2900776e87ef248d877a191044
ywwl <mifang@ywwl.com>
2018-09-27 20:58:24 +0800

    sf

--- ioc-test/src/main/java/DubboDemo/App.java
+++ ioc-test/src/main/java/DubboDemo/App.java
@@ -17,6 +17,10 @@ public class App
      */
     public static void main( String[] args )
     {
+<<<<<<< Updated upstream
+=======
+        //bug bug
+>>>>>>> Stashed changes
         int age = 12;
         System.out.println( "Hello World!" );
         int i=10;
